7d1c929f590bf456293f352ebb7921bb5ad4d169
sparanoid <t@sparanoid.com>
2022-09-07 16:21:48 -0400

    feat: enable contribute links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/hyperai/tvm-cn/edit/master/',
         },
         blog: {
           showReadingTime: true,
