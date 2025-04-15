45ed1254033d80d65efa30313cd197b6d8fe57e4
sparanoid <t@sparanoid.com>
2025-04-15 04:47:00 -0700

    chore(deps): add @docusaurus/faster and @docusaurus/types dependencies, enable experimental faster build in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,6 +15,10 @@ const config: Config = {
   organizationName: 'hyperai', // Usually your GitHub org/user name.
   projectName: 'tvm-cn', // Usually your repo name.
 
+  future: {
+    experimental_faster: true,
+  },
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
